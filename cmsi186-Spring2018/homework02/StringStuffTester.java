fc3d3a782d40f715e6cf45fbf1ce941ef77e2659
B.J. Johnson <bjohnson05@gmail.com>
2018-02-03 16:13:56 -0800

    minor modifications to correct test case required text

--- cmsi186-Spring2018/homework02/StringStuffTester.java
+++ cmsi186-Spring2018/homework02/StringStuffTester.java
@@ -311,8 +311,8 @@ public class StringStuffTester {
       try { System.out.println( (0 == StringStuff.oddsOnlyNoDupes( "" ).compareTo("")) ? "got it" : "don't got it" ); }
       catch( Exception e ) { System.out.println ( false ); }
 
-      System.out.print( "   Testing 'California' should return aio: " );
-      try { System.out.println( (0 == StringStuff.oddsOnlyNoDupes( "California" ).compareTo("aio")) ? "got it" : "don't got it" ); }
+      System.out.print( "   Testing 'California' should return Caio: " );
+      try { System.out.println( (0 == StringStuff.oddsOnlyNoDupes( "California" ).compareTo("Caio")) ? "got it" : "don't got it" ); }
       catch( Exception e ) { System.out.println ( false ); }
 
       System.out.print( "   Testing 'BDFHJLNPRTVXBDFHJLNPRTVX' should return the empty string: " );
